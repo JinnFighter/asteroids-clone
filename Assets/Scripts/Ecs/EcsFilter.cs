c52da4972ee59b760e508e96cb08040e0af1dc6b
Igor Budarin <i.budarin@kefirgames.com>
2021-10-13 13:11:01 +0300

    add: EcsFilter.Exclude methods #3

--- Assets/Scripts/Ecs/EcsFilter.cs
+++ Assets/Scripts/Ecs/EcsFilter.cs
@@ -1,11 +1,16 @@
 using System.Collections.Generic;
+using System.Linq;
 
 public class EcsFilter
 {
-    private List<EcsEntity> _entities;
+    private readonly List<EcsEntity> _entities;
 
     public EcsFilter(IEnumerable<EcsEntity> entities)
     {
         _entities = new List<EcsEntity>(entities);
     }
+
+    public EcsFilter Exclude<T>() => new EcsFilter(_entities.Where(entity => !entity.HasComponent<T>()));
+    
+    public EcsFilter Exclude<T, T1>() => new EcsFilter(_entities.Where(entity => !(entity.HasComponent<T>() || entity.HasComponent<T1>())));
 }

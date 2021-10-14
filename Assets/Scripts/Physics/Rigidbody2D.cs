01db52749aa9bc380cf931f2a95de9fac18fbacf
Igor Budarin <i.budarin@kefirgames.com>
2021-10-14 13:35:17 +0300

    chg: Rigidbody2D is now a class #10

--- Assets/Scripts/Physics/Rigidbody2D.cs
+++ Assets/Scripts/Physics/Rigidbody2D.cs
@@ -1,11 +1,20 @@
 namespace Physics
 {
-    public struct Rigidbody2D
+    public class Rigidbody2D
     {
         public Vector2 Position;
         public Vector2 Velocity;
         public Vector2 Force;
         public float Mass;
         public bool UseGravity;
+
+        public Rigidbody2D()
+        {
+            Position = Vector2.Zero;
+            Velocity = Vector2.Zero;
+            Force = Vector2.Zero;
+            Mass = 1f;
+            UseGravity = false;
+        }
     }
 }

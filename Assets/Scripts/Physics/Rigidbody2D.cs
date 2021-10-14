2067bfe7b66ffa6d75939c43dc15de1f6776b881
Igor Budarin <i.budarin@kefirgames.com>
2021-10-14 12:59:56 +0300

    add: Rigidbody2D struct #10

---
+++ Assets/Scripts/Physics/Rigidbody2D.cs
@@ -0,0 +1,11 @@
+namespace Physics
+{
+    public struct Rigidbody2D
+    {
+        public Vector2 Position;
+        public Vector2 Velocity;
+        public Vector2 Force;
+        public float Mass;
+        public bool UseGravity;
+    }
+}

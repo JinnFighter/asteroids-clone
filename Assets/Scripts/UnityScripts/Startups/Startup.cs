3dfa2cd38236bc571792b12f7c415064248c5388
Igor Budarin <i.budarin@kefirgames.com>
2021-10-15 18:59:51 +0300

    add: Startup now adds ShipGameObjectConveyor as service #17

--- Assets/Scripts/UnityScripts/Startups/Startup.cs
+++ Assets/Scripts/UnityScripts/Startups/Startup.cs
@@ -1,17 +1,24 @@
 using Logic;
+using Logic.Conveyors;
 using UnityEngine;
+using UnityScripts.Conveyors;
+using UnityScripts.Views;
 
 namespace UnityScripts.Startups
 {
     public class Startup : MonoBehaviour
     {
         private RuntimeCore _runtimeCore;
-        
+
+        [SerializeField] private GameObject _shipView;
+
         // Start is called before the first frame update
         void Start()
         {
             _runtimeCore = new RuntimeCore();
             _runtimeCore.Setup();
+            var shipConveyor = _runtimeCore.GetService<ShipConveyor>();
+            shipConveyor.AddNextConveyor(new ShipGameObjectConveyor(_shipView.GetComponent<PhysicsBodyView>()));
             _runtimeCore.Init();
         }
 

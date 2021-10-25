0ed1625a2e8ec43ae42789fed229b89bed8aea53
Igor Budarin <i.budarin@kefirgames.com>
2021-10-25 11:08:34 +0300

    add: PlayerEntitiesDataContainer #33

---
+++ Assets/Scripts/UnityScripts/Containers/PlayerEntitiesDataContainer.cs
@@ -0,0 +1,10 @@
+using DataContainers;
+using Ecs;
+using UnityEngine.InputSystem;
+
+namespace UnityScripts.Containers
+{
+    public class PlayerEntitiesDataContainer : DictionaryDataContainer<InputActionMap, EcsEntity>
+    {
+    }
+}

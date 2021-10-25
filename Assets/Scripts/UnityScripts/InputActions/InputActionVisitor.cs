b666f5010f6ecc4934eedd469cc8134d637fec03
Igor Budarin <i.budarin@kefirgames.com>
2021-10-25 18:29:20 +0300

    add: InputActionVisitor.CreateCommand method #33

--- Assets/Scripts/UnityScripts/InputActions/InputActionVisitor.cs
+++ Assets/Scripts/UnityScripts/InputActions/InputActionVisitor.cs
@@ -1,4 +1,5 @@
 using Logic.EventAttachers;
+using Logic.InputCommands;
 
 namespace UnityScripts.InputActions
 {
@@ -13,5 +14,7 @@ namespace UnityScripts.InputActions
         
         public void AttachEvent<T>(object sender, in T obj) where T : struct =>
             _eventAttacher.AttachEvent(sender, obj);
+
+        public IInputCommand CreateCommand<T>(object sender, in T obj) where T : struct => null;
     }
 }

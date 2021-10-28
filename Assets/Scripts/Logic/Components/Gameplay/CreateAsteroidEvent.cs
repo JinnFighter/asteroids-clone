0b05e9217af43fd7ee4c1541e17b1c532067a0b9
Igor Budarin <i.budarin@kefirgames.com>
2021-10-28 10:58:27 +0300

    add: CreateAsteroidEvent.Stage field #34

--- Assets/Scripts/Logic/Components/Gameplay/CreateAsteroidEvent.cs
+++ Assets/Scripts/Logic/Components/Gameplay/CreateAsteroidEvent.cs
@@ -4,6 +4,7 @@ namespace Logic.Components.Gameplay
 {
     public struct CreateAsteroidEvent
     {
+        public int Stage;
         public Vector2 Position;
         public float Mass;
         public Vector2 Direction;

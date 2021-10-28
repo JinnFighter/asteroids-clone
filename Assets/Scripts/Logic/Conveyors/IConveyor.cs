33cfd16bd3f536f3d862f8e9420fe6c35175793c
Igor Budarin <i.budarin@kefirgames.com>
2021-10-28 11:11:58 +0300

    add: IConveyor interface implementation #34

--- Assets/Scripts/Logic/Conveyors/IConveyor.cs
+++ Assets/Scripts/Logic/Conveyors/IConveyor.cs
@@ -5,4 +5,10 @@ namespace Logic.Conveyors
         void UpdateItem(T item);
         IConveyor<T> AddNextConveyor(IConveyor<T> nextConveyor);
     }
+    
+    public interface IConveyor<T, T1>
+    {
+        void UpdateItem(T item, T1 param);
+        IConveyor<T, T1> AddNextConveyor(IConveyor<T, T1> nextConveyor);
+    }
 }

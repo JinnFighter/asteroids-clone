8c403d7a7c34fa7d4da0ae49cf5116a663a25a99
Igor Budarin <i.budarin@kefirgames.com>
2021-10-28 17:30:27 +0300

    add: Vector2.Length property #42

--- Assets/Scripts/Physics/Vector2.cs
+++ Assets/Scripts/Physics/Vector2.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Physics
 {
     public readonly struct Vector2
@@ -50,5 +52,7 @@ namespace Physics
         public static Vector2 Zero => _zero;
 
         public static Vector2 One => _one;
+
+        public float Length => (float)Math.Sqrt(X * X + Y * Y);
     }
 }

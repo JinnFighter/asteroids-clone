5dd5fd762a16f0d57c832408924921e44cbdeced
Igor Budarin <i.budarin@kefirgames.com>
2021-10-29 17:14:33 +0300

    chg: LookInputActionConverter now converts from screen pointer position to world #45

--- Assets/Scripts/UnityScripts/InputActions/LookInputActionConverter.cs
+++ Assets/Scripts/UnityScripts/InputActions/LookInputActionConverter.cs
@@ -8,7 +8,8 @@ namespace UnityScripts.InputActions
     {
         public void AcceptConverter(InputActionVisitor visitor, InputAction inputAction)
         {
-            var point = inputAction.ReadValue<Vector2>();
+            var inputValue = inputAction.ReadValue<Vector2>();
+            var point = Camera.main.ScreenToWorldPoint(inputValue);
             var res = new LookInputAction { LookAtPoint = new Physics.Vector2(point.x, point.y) };
             visitor.AttachEvent(inputAction.actionMap, res);
         }

b6f25f182f9b01b3d4b51dbd80b0d71b4fd22b0e
Igor Budarin <i.budarin@kefirgames.com>
2021-11-04 10:33:23 +0300

    add: CollisionPoints struct #35

---
+++ Assets/Scripts/Physics/CollisionPoints.cs
@@ -0,0 +1,10 @@
+namespace Physics
+{
+    public struct CollisionPoints
+    {
+        public Vector2 A;
+        public Vector2 B;
+        public Vector2 Normal;
+        public float Depth;
+    }
+}

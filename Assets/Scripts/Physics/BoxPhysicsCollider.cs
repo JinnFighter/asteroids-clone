fa8103e2e1259471a3e4ac98b92e2851db6e369a
Igor Budarin <i.budarin@kefirgames.com>
2021-11-05 17:31:32 +0300

    add: PhysicsCollider.UpdatePosition method #35

--- Assets/Scripts/Physics/BoxPhysicsCollider.cs
+++ Assets/Scripts/Physics/BoxPhysicsCollider.cs
@@ -4,15 +4,25 @@ namespace Physics
     {
         public Vector2 TopLeft { get; private set; }
         public Vector2 DownRight { get; private set; }
+        
+        public float Width { get; private set; }
+        public float Height { get; private set; }
 
         public BoxPhysicsCollider(Vector2 position, float width, float height)
         {
-            var halfWidth = width / 2;
-            var halfHeight = height / 2;
+            Width = width;
+            Height = height;
+            UpdatePosition(position);
+        }
+
+        public sealed override void UpdatePosition(Vector2 position)
+        {
+            var halfWidth = Width / 2;
+            var halfHeight = Height / 2;
             TopLeft = new Vector2(position.X - halfWidth, position.Y - halfHeight);
             DownRight = new Vector2(position.X + halfWidth, position.Y + halfHeight);
         }
-        
+
         public override bool HasCollision(Vector2 position, PhysicsCollider other,
             Vector2 otherPosition)
             => other.HasCollisionWithBox(otherPosition, this, position);
--- Assets/Scripts/Physics/PhysicsCollider.cs
+++ Assets/Scripts/Physics/PhysicsCollider.cs
@@ -2,6 +2,7 @@ namespace Physics
 {
     public abstract class PhysicsCollider
     {
+        public abstract void UpdatePosition(Vector2 position);
         public abstract bool HasCollision(Vector2 position, PhysicsCollider other, Vector2 otherPosition);
         
         public abstract bool HasCollisionWithBox(Vector2 position, BoxPhysicsCollider other, Vector2 otherPosition);

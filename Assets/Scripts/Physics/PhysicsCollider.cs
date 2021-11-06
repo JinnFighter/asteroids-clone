cd1112f38b995e54632276cb7454ed1499315ee7
Igor Budarin <i.budarin@kefirgames.com>
2021-11-06 22:04:56 +0300

    add: PhysicsCollider #35

---
+++ Assets/Scripts/Physics/PhysicsCollider.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Physics
+{
+    public abstract class PhysicsCollider
+    {
+        private readonly HashSet<int> _collisionLayers; // layers collider is on
+        private readonly HashSet<int> _targetCollisionLayers; // layers collider checks collision with
+
+        protected PhysicsCollider(IEnumerable<int> collisionLayers, HashSet<int> targetCollisionLayers)
+        {
+            _targetCollisionLayers = targetCollisionLayers;
+            _collisionLayers = new HashSet<int>(collisionLayers);
+        }
+
+        public abstract void UpdatePosition(float x, float y);
+
+        public bool HasCollision(Vector2 position, PhysicsCollider other, Vector2 otherPosition) =>
+            other._collisionLayers.Intersect(_targetCollisionLayers).Any() &&
+            HasCollisionInternal(position, other, otherPosition);
+
+        protected abstract bool HasCollisionInternal(Vector2 position, PhysicsCollider other, Vector2 otherPosition);
+
+        public abstract bool HasCollisionWithBox(Vector2 position, BoxPhysicsCollider other, Vector2 otherPosition);
+    }
+}

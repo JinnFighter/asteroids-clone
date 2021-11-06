61230f93a0fb7ad3962d170bb607350aed71672d
Igor Budarin <i.budarin@kefirgames.com>
2021-11-06 22:25:01 +0300

    add: CheckShipCollisionsSystem #35

---
+++ Assets/Scripts/Logic/Systems/Gameplay/CheckShipCollisionsSystem.cs
@@ -0,0 +1,33 @@
+using System.Linq;
+using Ecs;
+using Ecs.Interfaces;
+using Logic.Components.Gameplay;
+using Logic.Components.Physics;
+using Physics;
+
+namespace Logic.Systems.Gameplay
+{
+    public sealed class CheckShipCollisionsSystem : IEcsRunSystem
+    {
+        private readonly CollisionsContainer _collisionsContainer;
+
+        public CheckShipCollisionsSystem(CollisionsContainer collisionsContainer)
+        {
+            _collisionsContainer = collisionsContainer;
+        }
+        
+        public void Run(EcsWorld ecsWorld)
+        {
+            var filter = ecsWorld.GetFilter<Ship, PhysicsBody>();
+            foreach (var index in filter)
+            {
+                var physicsBody = filter.Get2(index);
+                if (_collisionsContainer.GetData(physicsBody.Collider).Any())
+                {
+                    var entity = filter.GetEntity(index);
+                    entity.AddComponent(new GameOverEvent());
+                }
+            }
+        }
+    }
+}

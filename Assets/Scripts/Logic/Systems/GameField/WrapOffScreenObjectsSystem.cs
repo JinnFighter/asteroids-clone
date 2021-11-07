e563ce9bf22a6ee1f9012c5906faec63e568d352
Igor Budarin <i.budarin@kefirgames.com>
2021-11-07 16:46:51 +0300

    chg: WrapOffScreenObjectsSystem now uses Wrappable component #44

--- Assets/Scripts/Logic/Systems/GameField/WrapOffScreenObjectsSystem.cs
+++ Assets/Scripts/Logic/Systems/GameField/WrapOffScreenObjectsSystem.cs
@@ -18,7 +18,7 @@ namespace Logic.Systems.GameField
         
         public void Run(EcsWorld ecsWorld)
         {
-            var filter = ecsWorld.GetFilter<PhysicsBody>();
+            var filter = ecsWorld.GetFilter<PhysicsBody, Wrappable>();
 
             var topLeft = _gameFieldConfig.TopLeft;
             var downRight = _gameFieldConfig.DownRight;
@@ -26,31 +26,44 @@ namespace Logic.Systems.GameField
             foreach (var index in filter)
             {
                 var physicsBody = filter.Get1(index);
+                ref var wrappable = ref filter.Get2(index);
                 var transform = physicsBody.Transform;
                 var oldPosition = transform.Position;
                 var x = oldPosition.X;
                 var y = oldPosition.Y;
-                var isWrappingX = TryWrap(x, topLeft.X, downRight.X, out x);
-                var isWrappingY = TryWrap(y, topLeft.Y, downRight.Y, out y);
-                
-                transform.Position = new Vector2(x, y);
-                if (isWrappingX || isWrappingY)
+
+                var isInsideX = IsInside(x, topLeft.X, downRight.X);
+                var isInsideY = IsInside(y, topLeft.Y, downRight.Y);
+                if (isInsideX && isInsideY)
                 {
-                    var entity = filter.GetEntity(index);
-                    entity.AddComponent(new IsWrapped{ IsWrappingX = isWrappingX, IsWrappingY = isWrappingY });
+                    wrappable.IsWrappingX = false;
+                    wrappable.IsWrappingY = false;
                 }
+                else if (!wrappable.IsWrappingX || !wrappable.IsWrappingY)
+                {
+                    wrappable.IsWrappingX = TryWrap(wrappable.IsWrappingX, x, isInsideX, out x);
+                    wrappable.IsWrappingY = TryWrap(wrappable.IsWrappingY, y, isInsideY, out y);
+                }
+
+                transform.Position = new Vector2(x, y);
             }
         }
-        
-        private bool TryWrap(float val, float begin, float end, out float wrappedVal)
+
+        private bool TryWrap(bool isWrapping, float val, bool isInside, out float wrappedVal)
         {
-            var isOutside = val < begin || val > end;
-            if (isOutside)
+            var res = isWrapping;
+            var canWrap = !isWrapping && !isInside;
+            if (canWrap)
+            {
                 wrappedVal = -val;
+                res = true;
+            }
             else
                 wrappedVal = val;
 
-            return isOutside;
+            return res;
         }
+        
+        private bool IsInside(float val, float begin, float end) => val >= begin && val <= end;
     }
 }

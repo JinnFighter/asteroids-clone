c0e322d803b8bc0dbde9a8fcb1d76160fb5730ab
Igor Budarin <i.budarin@kefirgames.com>
2021-11-09 11:46:47 +0300

    chg: broken asteroids now can sometimes spawn new asteroids at same position #50

--- Assets/Scripts/Logic/Systems/Gameplay/DestroyAsteroidsSystem.cs
+++ Assets/Scripts/Logic/Systems/Gameplay/DestroyAsteroidsSystem.cs
@@ -1,6 +1,8 @@
+using System;
 using Ecs;
 using Ecs.Interfaces;
 using Logic.Components.Gameplay;
+using Logic.Components.Physics;
 
 namespace Logic.Systems.Gameplay
 {
@@ -8,11 +10,27 @@ namespace Logic.Systems.Gameplay
     {
         public void Run(EcsWorld ecsWorld)
         {
-            var filter = ecsWorld.GetFilter<Asteroid, DestroyEvent>();
+            var filter = ecsWorld.GetFilter<Asteroid, PhysicsBody, DestroyEvent>();
 
+            var random = new Random();
             foreach (var index in filter)
             {
                 var entity = filter.GetEntity(index);
+                var physicsBody = filter.Get2(index);
+                var transform = physicsBody.Transform;
+                var asteroid = filter.Get1(index);
+                var nextStage = asteroid.Stage--;
+                if (nextStage > 0 && random.Next(0, 2) > 0)
+                {
+                    entity.AddComponent(new CreateAsteroidEvent
+                    {
+                        Direction = transform.Position.Rotate(random.Next(15, 165)), 
+                        Mass = 10f, 
+                        Position = transform.Position, 
+                        Stage = nextStage
+                    });
+                }
+                
                 entity.RemoveComponent<Asteroid>();
             }
         }

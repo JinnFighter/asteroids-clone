8c6c60c0e75e19f872bb87dd5996911c9b277ab0
Igor Budarin <i.budarin@kefirgames.com>
2021-11-11 11:36:22 +0300

    add: creation of TransformBody presentation in BulletGameObjectFactory #65

--- Assets/Scripts/UnityScripts/Factories/BulletGameObjectFactory.cs
+++ Assets/Scripts/UnityScripts/Factories/BulletGameObjectFactory.cs
@@ -2,6 +2,9 @@ using Logic.Factories;
 using Physics;
 using UnityEngine;
 using UnityScripts.Containers;
+using UnityScripts.Presentation.Models;
+using UnityScripts.Presentation.Presenters;
+using UnityScripts.Presentation.Views;
 using Vector2 = Common.Vector2;
 
 namespace UnityScripts.Factories
@@ -10,6 +13,7 @@ namespace UnityScripts.Factories
     {
         private readonly PrefabsContainer _prefabsContainer;
         private readonly BulletFactory _wrappedFactory;
+        private GameObject _gameObject;
 
         public BulletGameObjectFactory(PrefabsContainer container, BulletFactory wrappedFactory)
         {
@@ -17,18 +21,26 @@ namespace UnityScripts.Factories
             _wrappedFactory = wrappedFactory;
         }
 
-        public override BodyTransform CreateTransform(Vector2 position, float rotation, Vector2 direction) =>
-            _wrappedFactory.CreateTransform(position, rotation, direction);
+        public override BodyTransform CreateTransform(Vector2 position, float rotation, Vector2 direction)
+        {
+            var transform = _wrappedFactory.CreateTransform(position, rotation, direction);
+            _gameObject = Object.Instantiate(_prefabsContainer.BulletPrefab, new UnityEngine.Vector2(position.X, position.Y),
+                Quaternion.identity);
+                
+            var physicsBodyModel = new TransformBodyModel(position.X, position.Y);
+            transform.PositionChangedEvent += physicsBodyModel.UpdatePosition;
+            transform.RotationChangedEvent += physicsBodyModel.UpdateRotation;
+            transform.DestroyEvent += physicsBodyModel.Destroy;
+            var presenter = new TransformBodyPresenter(physicsBodyModel, _gameObject.GetComponent<TransformBodyView>());
+            return transform;
+        }
 
         public override PhysicsRigidBody CreateRigidBody(float mass, bool useGravity) =>
             _wrappedFactory.CreateRigidBody(mass, useGravity);
 
         public override PhysicsCollider CreateCollider(Vector2 position)
         {
-            var gameObject = Object.Instantiate(_prefabsContainer.BulletPrefab, new UnityEngine.Vector2(position.X, position.Y),
-                Quaternion.identity);
-            
-            var spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+            var spriteRenderer = _gameObject.GetComponent<SpriteRenderer>();
             var rect = spriteRenderer.sprite.bounds;
             var size = rect.size;
             return new BoxPhysicsCollider(position, size.x, size.y);

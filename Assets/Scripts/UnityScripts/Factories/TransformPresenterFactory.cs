6c0776de9f450fb13760de3f5d1181f427eb406c
Igor Budarin <i.budarin@kefirgames.com>
2021-11-11 16:14:12 +0300

    add: TransformPresenterFactory class #65

---
+++ Assets/Scripts/UnityScripts/Factories/TransformPresenterFactory.cs
@@ -0,0 +1,19 @@
+using Physics;
+using UnityScripts.Presentation.Models;
+using UnityScripts.Presentation.Presenters;
+using UnityScripts.Presentation.Views;
+
+namespace UnityScripts.Factories
+{
+    public class TransformPresenterFactory : ITransformPresenterFactory
+    {
+        public TransformBodyPresenter CreatePresenter(BodyTransform transform, ITransformBodyView view)
+        {
+            var physicsBodyModel = new TransformBodyModel(transform.Position.X, transform.Position.Y);
+            transform.PositionChangedEvent += physicsBodyModel.UpdatePosition;
+            transform.RotationChangedEvent += physicsBodyModel.UpdateRotation;
+            transform.DestroyEvent += physicsBodyModel.Destroy;
+            return new TransformBodyPresenter(physicsBodyModel, view);
+        }
+    }
+}

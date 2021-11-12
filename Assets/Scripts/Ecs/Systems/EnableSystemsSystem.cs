db8387cf7072185636a40aaadd60909d7c29cd18
Igor Budarin <i.budarin@kefirgames.com>
2021-11-12 13:37:17 +0300

    add: EnableSystemsSystem #79

---
+++ Assets/Scripts/Ecs/Systems/EnableSystemsSystem.cs
@@ -0,0 +1,26 @@
+using Ecs.Components;
+using Ecs.Interfaces;
+
+namespace Ecs.Systems
+{
+    public class EnableSystemsSystem : IEcsRunSystem
+    {
+        private readonly EcsSystems _systems;
+
+        public EnableSystemsSystem(EcsSystems systems)
+        {
+            _systems = systems;
+        }
+        
+        public void Run(EcsWorld ecsWorld)
+        {
+            var filter = ecsWorld.GetFilter<EnableSystemsEvent>();
+
+            foreach (var index in filter)
+            {
+                var enableEvent = filter.Get1(index);
+                _systems.SetRunSystemState(enableEvent.Tag, true);
+            }
+        }
+    }
+}

7e2c8a46321ac183ebad827b2def86f2e43adb72
Igor Budarin <i.budarin@kefirgames.com>
2021-11-13 20:51:23 +0300

    add: ShipGameObjectTransformHandler #85

---
+++ Assets/Scripts/UnityScripts/EventHandlers/ShipGameObjectTransformHandler.cs
@@ -0,0 +1,28 @@
+using Logic.Events;
+using Physics;
+using UnityScripts.Factories;
+
+namespace UnityScripts.EventHandlers
+{
+    public class ShipGameObjectTransformHandler : IEventHandler<BodyTransform>
+    {
+        private readonly GameObjectEventHandlerContainer _gameObjectEventHandlerContainer;
+        private readonly IGameObjectFactory _gameObjectFactory;
+
+        public ShipGameObjectTransformHandler(GameObjectEventHandlerContainer container, IGameObjectFactory gameObjectFactory)
+        {
+            _gameObjectEventHandlerContainer = container;
+            _gameObjectFactory = gameObjectFactory;
+        }
+    
+        public void OnCreateEvent(BodyTransform context)
+        {
+            var gameObject = _gameObjectFactory.CreateGameObject(context.Position);
+            _gameObjectEventHandlerContainer.OnCreateEvent(gameObject);
+        }
+
+        public void OnDestroyEvent(BodyTransform context)
+        {
+        }
+    }
+}

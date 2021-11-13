197e53c3aa610cb2fbaaca145c45d69fa671a2c5
Igor Budarin <i.budarin@kefirgames.com>
2021-11-13 14:19:09 +0300

    add: ShipUiTransformEventHandler #85

---
+++ Assets/Scripts/UnityScripts/EventHandlers/ShipUiTransformEventHandler.cs
@@ -0,0 +1,30 @@
+using Logic.Events;
+using Physics;
+using UnityEngine;
+using UnityScripts.Factories;
+using UnityScripts.Presentation.Views;
+
+namespace UnityScripts.EventHandlers
+{
+    public class ShipUiTransformEventHandler : IEventHandler<BodyTransform>
+    {
+        private readonly ITransformPresenterFactory _transformPresenterFactory;
+        private readonly GameObject _gameObject;
+
+        public ShipUiTransformEventHandler(ITransformPresenterFactory factory, GameObject gameObject)
+        {
+            _transformPresenterFactory = factory;
+            _gameObject = gameObject;
+        }
+        
+        public void OnCreateEvent(BodyTransform context)
+        {
+            var presenter =
+                _transformPresenterFactory.CreatePresenter(context, _gameObject.GetComponent<TransformBodyView>());
+        }
+
+        public void OnDestroyEvent(BodyTransform context)
+        {
+        }
+    }
+}

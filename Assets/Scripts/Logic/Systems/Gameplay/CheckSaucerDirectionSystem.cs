23a45b8eebd803927c9404a6e897723b9939cf7d
Igor Budarin <i.budarin@kefirgames.com>
2021-11-14 15:07:00 +0300

    add: CheckSaucerDirectionSystem #51

---
+++ Assets/Scripts/Logic/Systems/Gameplay/CheckSaucerDirectionSystem.cs
@@ -0,0 +1,28 @@
+using Ecs;
+using Ecs.Interfaces;
+using Logic.Components.Gameplay;
+using Logic.Components.Physics;
+
+namespace Logic.Systems.Gameplay
+{
+    public class CheckSaucerDirectionSystem : IEcsRunSystem
+    {
+        public void Run(EcsWorld ecsWorld)
+        {
+            var filter = ecsWorld.GetFilter<Saucer, PhysicsBody>();
+
+            foreach (var index in filter)
+            {
+                var saucer = filter.Get1(index);
+                var physicsBody = filter.Get2(index);
+
+                var targetTransform = saucer.TargetTransform;
+                var saucerTransform = physicsBody.Transform;
+                var nextDirection = targetTransform.Position - saucerTransform.Position;
+                saucerTransform.Direction = nextDirection;
+                var rigidBody = physicsBody.RigidBody;
+                rigidBody.Force += saucerTransform.Direction;
+            }
+        }
+    }
+}

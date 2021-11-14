95d67d448c561506c58e6f307182b826fdb340f2
Igor Budarin <i.budarin@kefirgames.com>
2021-11-14 14:56:39 +0300

    add: CreateSpawnSaucerEventSystem #51

---
+++ Assets/Scripts/Logic/Systems/Gameplay/CreateSpawnSaucerEventSystem.cs
@@ -0,0 +1,68 @@
+using System;
+using Common;
+using Ecs;
+using Ecs.Interfaces;
+using Helpers;
+using Logic.Components.Gameplay;
+using Logic.Components.Time;
+using Logic.Config;
+using Logic.Containers;
+
+namespace Logic.Systems.Gameplay
+{
+    public class CreateSpawnSaucerEventSystem : IEcsRunSystem
+    {
+        private readonly GameFieldConfig _gameFieldConfig;
+        private readonly IRandomizer _randomizer;
+        private readonly TargetTransformContainer _targetTransformContainer;
+
+        public CreateSpawnSaucerEventSystem(GameFieldConfig gameFieldConfig, IRandomizer randomizer,
+            TargetTransformContainer targetTransformContainer)
+        {
+            _gameFieldConfig = gameFieldConfig;
+            _randomizer = randomizer;
+            _targetTransformContainer = targetTransformContainer;
+        }
+        
+        public void Run(EcsWorld ecsWorld)
+        {
+            var filter = ecsWorld.GetFilter<SaucerSpawnerConfig, Timer, TimerEndEvent>();
+
+            foreach (var index in filter)
+            {
+                var entity = filter.GetEntity(index);
+
+                var topLeft = _gameFieldConfig.TopLeft;
+                var downRight = _gameFieldConfig.DownRight;
+
+                float x = _randomizer.Range((int)topLeft.X, (int)downRight.X);
+                float y = _randomizer.Range((int)topLeft.Y, (int)downRight.Y);
+
+                if (_randomizer.IsProc(50))
+                {
+                    x = Math.Abs(x - topLeft.X) <
+                        Math.Abs(x - downRight.X)
+                        ? topLeft.X
+                        : downRight.X;
+                }
+                else
+                {
+                    y = Math.Abs(y - topLeft.Y) <
+                        Math.Abs(y - downRight.Y)
+                        ? topLeft.Y
+                        : downRight.Y;
+                }
+
+                var position = new Vector2(x, y);
+
+                entity.AddComponent(new CreateSaucerEvent{ Position = position, TargetTransform = _targetTransformContainer.Transform });
+
+                ref var saucerSpawnerConfig = ref filter.Get1(index);
+                ref var timer = ref filter.Get2(index);
+
+                timer.CurrentTime = _randomizer.Range(saucerSpawnerConfig.MinTime, saucerSpawnerConfig.MaxTime);
+                entity.AddComponent(new Counting());
+            }
+        }
+    }
+}

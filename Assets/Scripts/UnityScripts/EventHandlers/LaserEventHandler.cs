eeeb17d0b955af68418d965ee2dbd2cbbbbb63c0
Igor Budarin <i.budarin@kefirgames.com>
2021-11-15 16:32:06 +0300

    add: LaserEventHandler now implements IEventHandler<LaserMagazine> interface #81

--- Assets/Scripts/UnityScripts/EventHandlers/LaserEventHandler.cs
+++ Assets/Scripts/UnityScripts/EventHandlers/LaserEventHandler.cs
@@ -1,12 +1,13 @@
 using Logic.Components.Gameplay;
 using Logic.Events;
+using Logic.Weapons;
 using UnityScripts.Presentation.Models;
 using UnityScripts.Presentation.Presenters;
 using UnityScripts.Presentation.Views;
 
 namespace UnityScripts.EventHandlers
 {
-    public class LaserEventHandler : IComponentEventHandler<ShootLaserEvent>, IComponentEventHandler<Laser>, IComponentEventHandler<ReloadLaserEvent>
+    public class LaserEventHandler : IEventHandler<LaserMagazine>, IComponentEventHandler<ShootLaserEvent>, IComponentEventHandler<Laser>, IComponentEventHandler<ReloadLaserEvent>
     {
         private LaserModel _laserModel;
         private LaserPresenter _laserPresenter;
@@ -27,5 +28,12 @@ namespace UnityScripts.EventHandlers
         }
 
         public void Handle(ref ReloadLaserEvent context) => _laserModel.UpdateAmmoCount(context.CurrentAmmo);
+        
+        public void Handle(LaserMagazine context)
+        {
+            _laserModel = new LaserModel(context.CurrentAmmo);
+            context.AmmoChangedEvent += _laserModel.UpdateAmmoCount;
+            _laserPresenter = new LaserPresenter(_laserModel, _laserView);
+        }
     }
 }

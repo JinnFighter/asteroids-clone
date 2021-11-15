160bf1834e0ad16474231a3b1006409e49c5fc89
Igor Budarin <i.budarin@kefirgames.com>
2021-11-15 18:18:19 +0300

    add: TimerPresenterHandler class #81

---
+++ Assets/Scripts/UnityScripts/EventHandlers/TimerPresenterHandler.cs
@@ -0,0 +1,24 @@
+using Helpers;
+using Logic.Events;
+using UnityScripts.Presentation.Models;
+using UnityScripts.Presentation.Views;
+
+namespace UnityScripts.EventHandlers
+{
+    public class TimerPresenterHandler : IEventHandler<GameplayTimer>
+    {
+        private readonly ITimerView _timerView;
+
+        public TimerPresenterHandler(ITimerView timerView)
+        {
+            _timerView = timerView;
+        }
+        
+        public void Handle(GameplayTimer context)
+        {
+            var timerModel = new TimerModel(context.StartTime);
+            context.StartTimeChangedEvent += timerModel.UpdateStartTime;
+            context.CurrentTimeChangedEvent += timerModel.UpdateTime;
+        }
+    }
+}

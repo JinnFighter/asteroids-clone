0caa05f147a4de4331fd76d8e063d9a6163984eb
Igor Budarin <i.budarin@kefirgames.com>
2021-11-15 15:50:40 +0300

    add: Laser.AmmoMagazine field #81

--- Assets/Scripts/Logic/Components/Gameplay/Laser.cs
+++ Assets/Scripts/Logic/Components/Gameplay/Laser.cs
@@ -1,8 +1,11 @@
+using Logic.Weapons;
+
 namespace Logic.Components.Gameplay
 {
     public struct Laser
     {
         public int CurrentAmmo;
         public int MaxAmmo;
+        public IAmmoMagazine AmmoMagazine;
     }
 }

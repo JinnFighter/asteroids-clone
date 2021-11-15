303b0e5ccb22f089fd282bf362d037aa44fd3f30
Igor Budarin <i.budarin@kefirgames.com>
2021-11-15 15:53:42 +0300

    add: Laser.AmmoMagazine field initialization #81

--- Assets/Scripts/Logic/Systems/Gameplay/CreateLaserSystem.cs
+++ Assets/Scripts/Logic/Systems/Gameplay/CreateLaserSystem.cs
@@ -3,6 +3,7 @@ using Ecs.Interfaces;
 using Logic.Components.Gameplay;
 using Logic.Components.Time;
 using Logic.Events;
+using Logic.Weapons;
 
 namespace Logic.Systems.Gameplay
 {
@@ -22,7 +23,8 @@ namespace Logic.Systems.Gameplay
             foreach (var index in filter)
             {
                 var entity = filter.GetEntity(index);
-                var laser = new Laser { CurrentAmmo = 0, MaxAmmo = 3 };
+                var laserAmmoMagazine = new LaserMagazine(0, 3);
+                var laser = new Laser { CurrentAmmo = 0, MaxAmmo = 3, AmmoMagazine = laserAmmoMagazine };
                 entity.AddComponent(laser);
                 _componentEventHandlerContainer.HandleEvent(ref laser);
                 entity.AddComponent(new Timer{ CurrentTime = 7f });

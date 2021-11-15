b3c6f3b4a0c5c757910480846d4e5e83e6171484
Igor Budarin <i.budarin@kefirgames.com>
2021-11-15 22:43:51 +0300

    add: DestroyLaserSystem #81

---
+++ Assets/Scripts/Logic/Systems/Gameplay/DestroyLaserSystem.cs
@@ -0,0 +1,20 @@
+using Ecs;
+using Ecs.Interfaces;
+using Logic.Components.Gameplay;
+
+namespace Logic.Systems.Gameplay
+{
+    public class DestroyLaserSystem : IEcsRunSystem
+    {
+        public void Run(EcsWorld ecsWorld)
+        {
+            var filter = ecsWorld.GetFilter<Laser, DestroyEvent>();
+
+            foreach (var index in filter)
+            {
+                var entity = filter.GetEntity(index);
+                entity.RemoveComponent<Laser>();
+            }
+        }
+    }
+}

fe4a8cbfbe8208d6c628f16919af77894757a37e
Igor Budarin <i.budarin@kefirgames.com>
2021-11-17 13:10:00 +0300

    add: IPlayerInputReceiver.RemoveInputEvent method #82

--- Assets/Scripts/Logic/Input/IPlayerInputReceiver.cs
+++ Assets/Scripts/Logic/Input/IPlayerInputReceiver.cs
@@ -3,5 +3,6 @@ namespace Logic.Input
     public interface IPlayerInputReceiver
     {
         void AcceptInputEvent<T>(T eventObject) where T : struct;
+        void RemoveInputEvent<T>() where T : struct;
     }
 }

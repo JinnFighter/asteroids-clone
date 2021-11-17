b21c2960a53a39fb84307dfae7413fc9939b4660
Igor Budarin <i.budarin@kefirgames.com>
2021-11-17 21:06:20 +0300

    chg: renamed HandlerContainer to HandlerContainerWrapper #93

---
+++ Assets/Scripts/Logic/Events/HandlerContainerWrapper.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+namespace Logic.Events
+{
+    public abstract class HandlerContainerWrapper<T>
+    {
+        private readonly Dictionary<Type, GameEventHandlerContainer<T>> _dictionary;
+
+        public HandlerContainerWrapper()
+        {
+            _dictionary = new Dictionary<Type, GameEventHandlerContainer<T>>();
+        }
+
+        public void HandleEvent<C>(T context) where C : struct => _dictionary[typeof(C)].OnCreateEvent(context);
+
+        public void AddHandlerContainer<C>(GameEventHandlerContainer<T> container) where C : struct =>
+            _dictionary[typeof(C)] = container;
+
+        public void Clear()
+        {
+            foreach (var container in _dictionary)
+                container.Value.Clear();
+            _dictionary.Clear();
+        }
+    }
+}

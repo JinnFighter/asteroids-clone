a2e1d8340a1ff457eacfd04292716e557875fc00
Igor Budarin <i.budarin@kefirgames.com>
2021-11-18 20:13:12 +0300

    add: CollisionLayersConfig

---
+++ Assets/Scripts/Logic/Config/CollisionLayersConfig.cs
@@ -0,0 +1,9 @@
+namespace Logic.Config
+{
+    public class CollisionLayersConfig
+    {
+        public string ShipsLayer { get; set; }
+        public string AsteroidsLayer { get; set; }
+        public string SaucersLayer { get; set; }
+    }
+}

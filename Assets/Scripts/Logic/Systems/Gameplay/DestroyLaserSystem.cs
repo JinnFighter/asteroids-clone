9960976cbd079fbc093778e51e1b2c354d3a1985
Igor Budarin <i.budarin@kefirgames.com>
2021-11-18 21:00:23 +0300

    fix: memory leak on DestroyLaserSystem

--- Assets/Scripts/Logic/Systems/Gameplay/DestroyLaserSystem.cs
+++ Assets/Scripts/Logic/Systems/Gameplay/DestroyLaserSystem.cs
@@ -2,6 +2,7 @@ using Ecs;
 using Ecs.Interfaces;
 using Logic.Components.Gameplay;
 using Logic.Components.Physics;
+using Logic.Components.Time;
 
 namespace Logic.Systems.Gameplay
 {
@@ -9,14 +10,17 @@ namespace Logic.Systems.Gameplay
     {
         public void Run(EcsWorld ecsWorld)
         {
-            var filter = ecsWorld.GetFilter<Laser, PhysicsBody, DestroyEvent>();
+            var filter = ecsWorld.GetFilter<Laser, Timer, PhysicsBody, DestroyEvent>();
 
             foreach (var index in filter)
             {
                 var entity = filter.GetEntity(index);
                 entity.RemoveComponent<Laser>();
-                var physicsBody = filter.Get2(index);
+                var physicsBody = filter.Get3(index);
                 physicsBody.Transform.Destroy();
+                entity.RemoveComponent<Timer>();
+                if(entity.HasComponent<Counting>())
+                    entity.RemoveComponent<Counting>();
             }
         }
     }
